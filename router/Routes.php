37df029bcbc2bddf9cd4f65d383e361153f3fb27
Guillermo Fernández <gafernandez2525@gmail.com>
2022-09-29 21:31:24 +0200

    Add auth routes

--- router/Routes.php
+++ router/Routes.php
@@ -3,6 +3,7 @@
 namespace Router;
 
 use App\Controllers\AuthController;
+use App\Controllers\RegisterController;
 use App\Factories\RouterFactory;
 
 class Routes
@@ -13,7 +14,9 @@ class Routes
 
         $router->get("/login", [AuthController::class, "index"])
             ->post("/login", [AuthController::class, "login"])
-            ->get("/logout", [AuthController::class, "logout"]);
+            ->get("/logout", [AuthController::class, "logout"])
+            ->get("/register", [RegisterController::class, "index"])
+            ->post("/register", [RegisterController::class, "register"]);
 
         $router->resolve($_SERVER["REQUEST_URI"], strtolower($_SERVER["REQUEST_METHOD"]));
     }
